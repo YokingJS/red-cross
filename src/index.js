617911ffbca558b553072f1676ca847b03cd4e3d
YokingJS <yezhen.ye@163.com>
2018-04-29 13:53:29 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Banner from './components/banner/index';
+import NavigatorButton from './components/navigatorButton/index';
+import SerarchArea from './components/serarchArea/index'
+import Donors from './components/donors/index';
+import FootInfo from './components/footInfo/index';
+
+const styles = {
+  page: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#F5F5F5'
+  }
+};
+
+class Page extends React.Component{
+  constructor(props, context) {
+    super(props, context);
+  }
+  render() {
+    return (
+      <div style={styles.page}>
+        <Banner style={{width: '100%'}}/>
+        <NavigatorButton />
+        <SerarchArea />
+        <Donors />
+        <FootInfo />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Page />, document.getElementById('root'));
+
+
+
+
+
