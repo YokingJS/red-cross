c9ce1366f280d55fd93fe892f9d2d2486986e470
yezhen <yezhen.yz@alibaba-inc.com>
2018-05-02 01:51:55 +0800

    部分接口通

--- src/back-stage/fill.js
+++ src/back-stage/fill.js
@@ -7,189 +7,285 @@ class Page extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      bannerFiles: [],
-      donorFiles: []
+      donorFiles: [],
+      title: '',
+      content: '',
+      imageUrl: '',
+      videoUrl: '',
+      name: '',
+      sex: '',
+      age: '',
+      disease: '',
+      mobile: '',
+      targetMoney: '',
+      deployDepartment: '',
+      currentMoney: '',
+      donatorNum: '',
+      followUrl: ''
     };
     this.renderButton = this.renderButton.bind(this);
-    this.onBannerChange = this.onBannerChange.bind(this);
+    this.onHelpImgChange = this.onHelpImgChange.bind(this);
+    this.onChangeInput = this.onChangeInput.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onBannerChange(bannerFiles, type, index) {
+  onHelpImgChange(donorFiles, type, index) {
     this.setState({
-      bannerFiles,
+      donorFiles,
+      imageUrl: donorFiles[0].url
     });
+  }
+
+  onSubmit() {
+    console.log(this.state);
+  }
+
+  onChangeInput(item) {
+    let onChangeInput = (value) => {
+      switch(item) {
+        case 'title':
+          this.state.title = value;
+          break;
+        case 'content':
+          this.state.content = value;
+          break;
+        case 'name':
+          this.state.name = value;
+          break;
+        case 'sex':
+          this.state.sex = value;
+          break;
+        case 'age':
+          this.state.age = value;
+          break;
+        case 'disease':
+          this.state.disease = value;
+          break;
+        case 'videoUrl':
+          this.state.videoUrl = value;
+          break;
+        case 'mobile':
+          this.state.mobile = value;
+          break;
+        case 'targetMoney':
+          this.state.targetMoney = value;
+          break;
+        case 'deployDepartment':
+          this.state.deployDepartment = value;
+          break;
+        case 'currentMoney':
+          this.state.currentMoney = value;
+          break;
+        case 'donatorNum':
+          this.state.donatorNum = value;
+          break;
+        case 'followUrl': 
+          this.state.followUrl = value;
+        default: break;
+      }
+      this.setState({
+        ...this.state
+      });
+    };
+
+    return onChangeInput;
+
   }
 
   renderButton() {
     return (
       <div style={styles.fixedButton}>
-        <div style={styles.helpButton} onClick={this.onSubmit}>爱心提交</div>
+        <div style={styles.helpButton} onClick={this.onSubmit}>发布</div>
       </div>
     );
   }
   
   render() {
-    const { bannerFiles = [], donorFiles = [] } = this.state || {};
+    const { donorFiles = [], title = '', content = '', imageUrl = '', videoUrl = '', name = '',
+      sex = '', age = '', disease = '', mobile = '', targetMoney = '', deployDepartment = '',
+      currentMoney = '', donatorNum = '' , followUrl = ''
+    } = this.state || {};
+    const MARGINTOP = '10px';
+    const TITLEWIDTH = '100px';
+    const IMAGEWIDTH = '90px';
     return (
       <div style={styles.fillInfo}>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
-          <span style={{...styles.largeText, color: 'transparent'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>捐赠总金额:</span>
-          <span style={{...styles.largeText, ...styles.boxWithBorder, ...styles.textOverflow}}>333元</span>
-        </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
-          <span style={{...styles.largeText, color: 'transparent'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>捐赠总人数:</span>
-          <span style={{...styles.largeText, ...styles.boxWithBorder, ...styles.textOverflow}}>333人</span>
-        </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem', height: '20rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>banner图片:</span>
-          <div style={{...styles.boxWithBorder, height: '20rem', width: '20rem'}} className="pickerBox">
-            <ImagePicker
-              files={bannerFiles}
-              onChange={this.onBannerChange}
-              selectable={bannerFiles.length < 1}
-            />
-          </div>
-        </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
-          <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>标题:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>标题:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
+              value={title}
+              onChange={this.onChangeInput('title')}
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>求助信息:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>求助信息:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
+              value={content}
+              onChange={this.onChangeInput('content')}
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem', height: '20rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP, height: IMAGEWIDTH}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>求助图片:</span>
-          <div style={{...styles.boxWithBorder, height: '20rem', width: '20rem'}} className="pickerBox">
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>求助图片:</span>
+          <div style={{...styles.boxWithBorder, height: IMAGEWIDTH, width: IMAGEWIDTH}} className="pickerBox">
             <ImagePicker
               files={donorFiles}
-              onChange={this.onImageChange}
+              onChange={this.onHelpImgChange}
               selectable={donorFiles.length < 1}
             />
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>求助视频:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>求助视频:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
+              value={videoUrl}
+              onChange={this.onChangeInput('videoUrl')}
               placeholder="请正确填写url，否则链接无法访问"
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>救助人姓名:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>救助人姓名:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
+              value={name}
+              onChange={this.onChangeInput('name')}
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>救助人性别:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>救助人性别:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
               placeholder="男/女"
+              value={sex}
+              onChange={this.onChangeInput('sex')}
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>联系电话:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>联系电话:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
               placeholder="请输入手机号"
+              value={mobile}
+              onChange={this.onChangeInput('mobile')}
               type="phone"
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>救助人年龄:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>救助人年龄:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
               type="number"
+              value={age}
+              onChange={this.onChangeInput('age')}
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>已筹金额:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>病种:</span>
+          <div style={styles.boxWithBorder} className="pickerBox">
+            <InputItem
+              clear
+              value={disease}
+              onChange={this.onChangeInput('disease')}
+            ></InputItem>
+          </div>
+        </div>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
+          <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>目标金额:</span>
+          <div style={styles.boxWithBorder} className="pickerBox">
+            <InputItem
+              clear
+              type="targetMoney"
+              value={content}
+              onChange={this.onChangeInput('targetMoney')}
+            ></InputItem>
+          </div>
+        </div>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
+          <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>已筹金额:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
               type="money"
+              value={currentMoney}
+              onChange={this.onChangeInput('currentMoney')}
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>捐款人数:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>捐款人数:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
               type="number"
+              value={donatorNum}
+              onChange={this.onChangeInput('donatorNum')}
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
-          <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>项目状态:</span>
-          <div style={styles.boxWithBorder} className="pickerBox">
-            <InputItem
-              clear
-              placeholder="输入0~2;0未开始;1进行中;2已结束"
-              type="number"
-            ></InputItem>
-          </div>
+        <div style={{...styles.rowLine, marginTop: '35px'}}>
+          <span style={{...styles.largeText, color: 'transparent'}}>*</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>项目状态:</span>
+          <span style={{...styles.largeText, ...styles.boxNoBorder, ...styles.textOverflow}}>未发布</span>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>发布单位:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>发布单位:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
               placeholder=""
+              value={deployDepartment}
+              onChange={this.onChangeInput('deployDepartment')}
             ></InputItem>
           </div>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
-          <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>发布时间:</span>
-          <span style={{...styles.largeText, ...styles.boxWithBorder, ...styles.textOverflow}}>自动获取发布点击时的时间</span>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
+          <span style={{...styles.largeText, color: 'transparent'}}>*</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>发布时间:</span>
+          <span style={{...styles.largeText, ...styles.boxNoBorder, ...styles.textOverflow}}>将获取点击“发布”时的时间</span>
         </div>
-        <div style={{...styles.rowLine, marginTop: '3.5rem'}}>
+        <div style={{...styles.rowLine, marginTop: MARGINTOP}}>
           <span style={{...styles.largeText, color: '#ff3322'}}>*</span>
-          <span style={{...styles.largeText, width: '28rem', textAlign: 'right'}}>项目跟进:</span>
+          <span style={{...styles.largeText, width: TITLEWIDTH, textAlign: 'right'}}>项目跟进:</span>
           <div style={styles.boxWithBorder} className="pickerBox">
             <InputItem
               clear
               placeholder="请正确填写url，否则跟进链接无法访问"
+              value={followUrl}
+              onChange={this.onChangeInput('followUrl')}
             ></InputItem>
           </div>
         </div>
         {this.renderButton()}
-        <div style={{height: '19rem'}}/>
+        <div style={{height: '190px'}}/>
       </div>
     );
   }
@@ -211,25 +307,56 @@ const styles = {
   },
   rowLine: {
     flex: 1,
-    height: '10rem',
+    height: '40px',
     display: 'flex',
     flexDirection: 'row',
-    padding: '0 4rem',
+    padding: '0 5px',
     alignItems: 'center'
   },
   boxWithBorder: {
-    marginLeft: '2rem',
+    marginLeft: '5px',
     border: '1px solid #999999',
-    borderRadius: '1rem',
-    height: '10rem',
+    borderRadius: '4px',
+    height: '40px',
     flex: 1,
-    padding: '0 2rem',
+    padding: '0 5px',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  boxNoBorder: {
+    marginLeft: '5px',
+    height: '40px',
+    flex: 1,
+    padding: '0 5px',
     display: 'flex',
     alignItems: 'center'
   },
   largeText: {
-    lineHeight: '5rem',
-    fontSize: '4.8rem',
+    lineHeight: '50px',
+    fontSize: '12px',
     color: '#666666'
+  },
+  fixedButton: {
+    position: 'fixed',
+    width: '100%',
+    padding: '0rem 10px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100px',
+    left: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  helpButton: {
+    width: '200px',
+    height: '60px',
+    lineHeight: '60px',
+    fontSize: '20px',
+    color: '#fff',
+    backgroundColor: '#fd6463',
+    textAlign: 'center',
+    borderRadius: '6px'
   }
 };
